8460fc3a70b13f02b350cfd00b29d6d29cb3b9e2
BrightSkyz <BrightSkyz@users.noreply.github.com>
2019-05-18 21:39:22 -0400

    Add MySQL to MOTD

--- src/main/java/io/brightskyz/complex/bungee/motd/MotdModule.java
+++ src/main/java/io/brightskyz/complex/bungee/motd/MotdModule.java
@@ -3,6 +3,10 @@ package io.brightskyz.complex.bungee.motd;
 import io.brightskyz.complex.Bungee;
 import io.brightskyz.complex.bungee.BungeeModule;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MotdModule extends BungeeModule {
 
     public MotdModule(Bungee bungee) {
@@ -11,7 +15,16 @@ public class MotdModule extends BungeeModule {
 
     @Override
     public void onEnable() {
-        getBungee().getCache().getShard("settings").set("motd", "&f         &9&m---&8&m&l[-&f  &6&lMineplex &f&lGames &8[&7US&8] &8&m&l-]&9&m---&r\n                     &c&lCLANS SEASON 6");
+        getBungee().getCache().getShard("settings").set("motd", "&f         &9&m---&8&m&l[-&f  &6&lComplex &f&lGames &8[&7US&8] &8&m&l-]&9&m---&r\n                     &c&lowo whats this");
+        try {
+            PreparedStatement statement = getBungee().getDatabaseConnection().prepareStatement("SELECT * FROM settings WHERE name = 'motd'");
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                getBungee().getCache().getShard("settings").set("motd", resultSet.getString("value"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         getBungee().getProxy().getPluginManager().registerListener(getBungee(), new ProxyPingListener(getBungee()));
     }
 }
